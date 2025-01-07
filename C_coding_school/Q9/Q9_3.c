970cf15c38d993fd522422c223b3aa30913018ad
goose-cobra7 <eranydin@gmail.com>
2025-01-07 18:05:40 +0200

    fix Q9_3 <->Q8_4 name switch

--- C_coding_school/Q9/Q9_3.c
+++ C_coding_school/Q9/Q9_3.c
@@ -2,19 +2,18 @@
 #include <string.h>
 #include <math.h>
 
-void HexToDecimal(char string[], char result[]);
+int HexToDecimal(char string[]);
 int main()
 {
     char string[1024];
-    char result[1024];
     printf("Enter a string: 0x");
     fgets(string, 1024, stdin); //"1024" number of characters in string, "stdin" standard input (keyboard input)
-    HexToDecimal(string, result);
-    printf("The decimal equivalent is: %s\n", result); //print the reversed string with upper case letters
+    int dec = HexToDecimal(string);
+    printf("The decimal value of the hexadecimal number is: %d\n", dec);
     return 0;
 }
 
-void HexToDecimal(char string[], char result[])
+int HexToDecimal(char string[])
 {
     int i = 0;
     while (string[i]!= '\0')
@@ -28,16 +27,11 @@ void HexToDecimal(char string[], char result[])
         i++; //run on string
     }
     //this way the value of the char turn to meaninless string but on the dec it have meaning
-    int j = 0,k = 0; //create "k" = counter, "j" = result
-    for (i = i-2; i >= 0; i--) //run from the end of the string to the start
+    int j = 0,k = 0;
+    for (i = i-2; i >= 0; i--) 
     {
-        j += string[k] * pow(16,i);// (A*16^2)+(B*16^1)+C
+        j += string[k] * pow(16,i);
         k++;
     }
-    result[1+k] = '\0'; //create end of string
-    while (j != 0) //insert the result to string format
-    {
-        result[k--] = (j % 10) + '0'; //turn it from "int" to "char" using ASCII
-        j /= 10;
-    }
+    return j;
 }
--- C_coding_school/Q9/Q9_4.c
+++ C_coding_school/Q9/Q9_4.c
@@ -2,18 +2,19 @@
 #include <string.h>
 #include <math.h>
 
-int HexToDecimal(char string[]);
+void HexToDecimal(char string[], char result[]);
 int main()
 {
     char string[1024];
+    char result[1024];
     printf("Enter a string: 0x");
     fgets(string, 1024, stdin); //"1024" number of characters in string, "stdin" standard input (keyboard input)
-    int dec = HexToDecimal(string);
-    printf("The decimal value of the hexadecimal number is: %d\n", dec);
+    HexToDecimal(string, result);
+    printf("The decimal equivalent is: %s\n", result); //print the reversed string with upper case letters
     return 0;
 }
 
-int HexToDecimal(char string[])
+void HexToDecimal(char string[], char result[])
 {
     int i = 0;
     while (string[i]!= '\0')
@@ -27,11 +28,16 @@ int HexToDecimal(char string[])
         i++; //run on string
     }
     //this way the value of the char turn to meaninless string but on the dec it have meaning
-    int j = 0,k = 0;
-    for (i = i-2; i >= 0; i--) 
+    int j = 0,k = 0; //create "k" = counter, "j" = result
+    for (i = i-2; i >= 0; i--) //run from the end of the string to the start
     {
-        j += string[k] * pow(16,i);
+        j += string[k] * pow(16,i);// (A*16^2)+(B*16^1)+C
         k++;
     }
-    return j;
+    result[1+k] = '\0'; //create end of string
+    while (j != 0) //insert the result to string format
+    {
+        result[k--] = (j % 10) + '0'; //turn it from "int" to "char" using ASCII
+        j /= 10;
+    }
 }
